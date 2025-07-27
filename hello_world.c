950a513ad8e2d0616f490007c3a1ea688a87e63c
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2025-07-27 11:20:26 -0700

    Create hello_world.c
    Maybe having some programming code will get this repository more attention by search engines? No idea.

---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// main is automatically called for applications.
+int main() {
+   // printf() displays the string inside quotation.
+   printf("Hello, World! RoundSparrow was here.");
+   // return excit code for app execution, 0 is normal exit, not an abend.
+   return 0;
+}
