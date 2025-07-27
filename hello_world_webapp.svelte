8bd8055ce12d2285b75f12e08c5f6ee34b4bdd46
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2025-07-27 11:37:47 -0700

    Create hello_world_webapp.svelte

---
+++ hello_world_webapp.svelte
@@ -0,0 +1,13 @@
+<script>
+	let name = $state('world');
+	let count = $state(0);
+</script>
+
+<h1>Hello {name}!</h1>
+
+No idea waht version of Svelte this works on.
+
+<input bind:value={name} />
+<button onclick={() => count += 1}>
+	clicks: {count}
+</button>
