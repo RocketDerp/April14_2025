605e46ccd66bbede4d1a1477946d48ea765b9b38
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2025-07-29 09:56:22 -0700

    ahh, the high school glory days in Fort Wayne Indiana

--- hello_world_Ada.adb
+++ hello_world_Ada.adb
@@ -1,6 +1,16 @@
+--
+-- Hello world, story about when I first used Ada:
+-- In 1986, they were teaching Ada software development
+--    at the Telos Federal Systems office satellite office
+--    in Fort Wayne, Indiana for software engineering
+--    for the United States Armed Forces / Pentagon.
+-- The Advanced Field Artillery Tactical Data System (AFATDS).
+--    Telos Frogs, "Pup", Apprentice
+-- 
+
 with Ada.Text_IO;
 
 procedure My_Hello_World is
 begin
-   Ada.Text_IO.Put_Line ("Hello, World! RoundSparrow was here.");
+   Ada.Text_IO.Put_Line ("Hello, World! RoundSparrow / Pup was here.");
 end My_Hello_World;
