fe628d523b8cde9ab56393cf7af2bc6603454ebe
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2025-07-29 09:58:06 -0700

    office office, reformat comments

--- hello_world_Ada.adb
+++ hello_world_Ada.adb
@@ -1,11 +1,12 @@
 --
 -- Hello world, story about when I first used Ada:
 -- In 1986, they were teaching Ada software development
---    at the Telos Federal Systems office satellite office
+--    at the Telos Federal Systems satellite office
 --    in Fort Wayne, Indiana for software engineering
 --    for the United States Armed Forces / Pentagon.
 -- The Advanced Field Artillery Tactical Data System (AFATDS).
---    Telos Frogs, "Pup", Apprentice
+--
+--          Telos Frogs, "Pup", Apprentice
 -- 
 
 with Ada.Text_IO;
