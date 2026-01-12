b773de2c2f46bc3eb2e1cc32ab043a5c817a1a3a
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-11 20:10:10 -0700

    Try every 5 slow down

--- Quotes_Pile/Reddit2026A/Reddit_comments_pull_ChatGPT_authored13_mod3.py
+++ Quotes_Pile/Reddit2026A/Reddit_comments_pull_ChatGPT_authored13_mod3.py
@@ -494,7 +494,7 @@ def fetch_user_info_hover(username, args=None):
         print(f"live-fetch for user_info_hover {url}")
         fetchRedditCountA += 1
         
-        slowdown_every = 6
+        slowdown_every = 5
         if fetchRedditCountA % slowdown_every == 0:
             print(f"fetchRedditCountA {fetchRedditCountA} is a multiple of {slowdown_every}.")
             sleep_time = random.uniform(9, 25)
