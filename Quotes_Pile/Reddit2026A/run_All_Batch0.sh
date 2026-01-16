d1c629b92d69ec0630e80983855bf9933a3fd3b1
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-16 08:18:10 -0700

    Update run_All_Batch0.sh

--- Quotes_Pile/Reddit2026A/run_All_Batch0.sh
+++ Quotes_Pile/Reddit2026A/run_All_Batch0.sh
@@ -25,4 +25,11 @@ fi
 
 ./run_World_Hate0.sh
 
+if [ $? -ne 0 ]; then
+    echo "Error: The sub-script run_Trump0.sh failed. Exiting main script." >&2
+    exit 1
+fi
+
+./run_Trump0.sh
+
 cat Output/run_summary0.txt
