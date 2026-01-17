16bca8db848fd57db5daec4617cc30fdfdbce9c0
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-16 18:09:59 -0700

    skip to 190

--- Quotes_Pile/Reddit2026A/dev_run_World_Hate_Affairs0.sh
+++ Quotes_Pile/Reddit2026A/dev_run_World_Hate_Affairs0.sh
@@ -2,4 +2,4 @@
 
 # python Reddit_comments_pull_ChatGPT_authored13_mod3_add1.py --file World_Hate_Affairs_2026-01-08.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 0 --stop-after 2
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3_add1.py --file World_Hate_Affairs_2026-01-08.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 0
+python Reddit_comments_pull_ChatGPT_authored13_mod3_add1.py --file World_Hate_Affairs_2026-01-08.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 190
