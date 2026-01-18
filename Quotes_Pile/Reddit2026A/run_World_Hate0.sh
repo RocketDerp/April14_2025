fee9a7f5535d374ca92f87a4e70c4fe1ef94a9f7
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-18 14:39:55 -0700

    Skip to 325

--- Quotes_Pile/Reddit2026A/run_World_Hate0.sh
+++ Quotes_Pile/Reddit2026A/run_World_Hate0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file World_Hate_Affairs_2026-01-08.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 0
+python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file World_Hate_Affairs_2026-01-08.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 325
