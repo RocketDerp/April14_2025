e040fd8f9dd35572ce4d502f7cf601a33d500feb
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-24 03:59:31 -0700

    Change files

--- Quotes_Pile/Reddit2026A/run_World_Hate0.sh
+++ Quotes_Pile/Reddit2026A/run_World_Hate0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file World_Hate_Affairs_2026-01-08.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 325
+python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file World_Hate_Affairs_2026-01-24.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 0
