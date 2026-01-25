25af795be1541d49964baba918a7c93c08cc2669
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-25 05:10:10 -0700

    new source file

--- Quotes_Pile/Reddit2026A/run_Trump0.sh
+++ Quotes_Pile/Reddit2026A/run_Trump0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file Trumpism_Reddit_comments_2026-01-16.md --output Output/Trumpism_out.md --check-saved-first --skip-to 0
+python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file Trumpism_Reddit_comments_2026-01-25.md --output Output/Trumpism_out.md --check-saved-first --skip-to 0
