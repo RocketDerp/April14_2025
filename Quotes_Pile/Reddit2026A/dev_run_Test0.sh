9e1130648f7834ac5ae42997b40bbf002c627471
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-25 09:11:33 -0700

    new Python filename convention

--- Quotes_Pile/Reddit2026A/dev_run_Test0.sh
+++ Quotes_Pile/Reddit2026A/dev_run_Test0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3_add1.py --file ./Test/test_input_list_links_commentary.md --output Output/Test_out.md --check-saved-first --skip-to 0 --stop-after 3
+python Old_Reddit_Pull_Markdown_dev0.py --file ./Test/test_input_list_links_commentary.md --output Output/Test_out.md --check-saved-first --skip-to 0 --stop-after 3
