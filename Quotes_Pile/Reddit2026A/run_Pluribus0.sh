5f0bdefb6362cb07e1d2d6009bdb172d85304c5b
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-25 09:44:24 -0700

    new Python filename convention Old_Reddit_Pull_Markdown.py

--- Quotes_Pile/Reddit2026A/run_Pluribus0.sh
+++ Quotes_Pile/Reddit2026A/run_Pluribus0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file Plur1bus_Reddit_comments_2026-01-01.md --output Output/Pluribus_out.md --check-saved-first --skip-to 0
+python Old_Reddit_Pull_Markdown.py --file Plur1bus_Reddit_comments_2026-01-01.md --output Output/Pluribus_out.md --check-saved-first --skip-to 0
