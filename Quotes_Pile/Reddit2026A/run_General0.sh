baf2ef0b6638c36912d54b3ec61e0df32ff6cb99
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-25 09:45:00 -0700

    new Python filename convention Old_Reddit_Pull_Markdown.py

--- Quotes_Pile/Reddit2026A/run_General0.sh
+++ Quotes_Pile/Reddit2026A/run_General0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file General_Reddit_comments_2026-01-15.md --output Output/General_out.md --check-saved-first --skip-to 0
+python Old_Reddit_Pull_Markdown.py --file General_Reddit_comments_2026-01-15.md --output Output/General_out.md --check-saved-first --skip-to 0
