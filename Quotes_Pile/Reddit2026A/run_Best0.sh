35a5aa232249043a94ab9d698a42157a54a7c644
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-25 09:46:06 -0700

    new Python filename convention Old_Reddit_Pull_Markdown.py

--- Quotes_Pile/Reddit2026A/run_Best0.sh
+++ Quotes_Pile/Reddit2026A/run_Best0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Reddit_comments_pull_ChatGPT_authored13_mod3.py --file Reddit_Best_of_2025_set0.md --output Output/Reddit_Best_of_2025_out.md --check-saved-first --skip-to 0
+python Old_Reddit_Pull_Markdown.py --file Reddit_Best_of_2025_set0.md --output Output/Reddit_Best_of_2025_out.md --check-saved-first --skip-to 0
