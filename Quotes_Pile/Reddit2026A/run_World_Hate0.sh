31f87194afe77694fffe6911b2b43ddd385697ff
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-28 16:06:29 -0700

    Change output filename

--- Quotes_Pile/Reddit2026A/run_World_Hate0.sh
+++ Quotes_Pile/Reddit2026A/run_World_Hate0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Old_Reddit_Pull_Markdown.py --file World_Hate_Affairs_2026-01-24.md --output Output/World_Hate_Affairs_out.md --check-saved-first --skip-to 0
+python Old_Reddit_Pull_Markdown.py --file World_Hate_Affairs_2026-01-24.md --output Output/Reddit_commentary_2026-01-24_World_Hate_Affairs_out.md --check-saved-first --skip-to 0
