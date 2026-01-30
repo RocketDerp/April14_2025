7b266d43a38dc7c8cbf933d7c9c8f21ffcef7662
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-01-30 08:51:35 -0700

    Update and rename run_Grab_Older0.sh to run_Older_Grab0.sh

--- Quotes_Pile/Reddit2026A/run_Older_Grab0.sh
+++ Quotes_Pile/Reddit2026A/run_Older_Grab0.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# this file is intentional named to make tab completion unique
 
 python Old_Reddit_Pull_Markdown.py --file Grab_Older_Reddit_comments_2026-01-26.md --output Output/Grab_Older_out.md --check-saved-first --skip-to 0
