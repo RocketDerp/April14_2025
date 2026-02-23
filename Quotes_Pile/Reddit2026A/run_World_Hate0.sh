12238e43d8ad0e6a01b029465a9b758cefbb489d
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-02-23 05:51:46 -0700

    update to latest file

--- Quotes_Pile/Reddit2026A/run_World_Hate0.sh
+++ Quotes_Pile/Reddit2026A/run_World_Hate0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Old_Reddit_Pull_Markdown.py --file World_Hate_Affairs_2026-02-10.md --output Output/Reddit_commentary_2026-02-10_World_Hate_Affairs_out.md --check-saved-first --skip-to 135
+python Old_Reddit_Pull_Markdown.py --file World_Hate_Affairs_2026-02-23.md --output Output/Reddit_commentary_2026-02-23_World_Hate_Affairs_out.md --check-saved-first --skip-to 0
