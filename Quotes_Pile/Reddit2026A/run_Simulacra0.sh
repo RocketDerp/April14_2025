540ec9a743c2ec783e33e44d44f4855ab0404780
RocketDerp <113625597+RocketDerp@users.noreply.github.com>
2026-02-24 11:20:16 -0700

    New file created today, change output filename convention

--- Quotes_Pile/Reddit2026A/run_Simulacra0.sh
+++ Quotes_Pile/Reddit2026A/run_Simulacra0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Old_Reddit_Pull_Markdown.py --file Simulacra_Resistance_Reddit_comments_2026-01-01.md --output Output/Simulacra_out.md --check-saved-first --skip-to 0
+python Old_Reddit_Pull_Markdown.py --file Simulacra_Resistance_Reddit_comments_2026-02-24.md --output Output/Reddit_commentary_2026-02-24_Simulacra_out.md --check-saved-first --skip-to 0
